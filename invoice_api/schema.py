04b2083ac7227324ef68bc41a0482dfbb11d9266
Dominique Rehborn <dominique@rehborn.dev>
2025-09-03 11:32:22 +0200

    refactor set empty list as default for positions, remove unused regex and comment

--- invoice_api/schema.py
+++ invoice_api/schema.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, Field
 from datetime import date
 
 SLUG = r"^[\w-]+$"
-DATE = r"^\d{4}-\d{2}-\d{2}$"
 
 
 class Company(BaseModel):
@@ -56,5 +55,4 @@ class Invoice(BaseModel):
 
     company: Company
     customer: Customer
-    # positions: list[InvoicePosition]
-    positions: Annotated[list[InvoicePosition], Len(min_length=1, max_length=100)] | None = None
+    positions: Annotated[list[InvoicePosition], Len(min_length=1, max_length=100)] = []
